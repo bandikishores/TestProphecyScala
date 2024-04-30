8e1c5b23d040fd28f456019f6d2be48828e424e1
bandikishores@gmail.com <bandikishores@gmail.com>
2024-04-30 16:59:30 +0000

    ❇️ New     - Pipeline        - TestScalaPipelineGit

---
+++ pipelines/TestScalaPipelineGit/code/src/main/scala/io/prophecy/pipelines/testscalapipelinegit/main.scala
@@ -0,0 +1,36 @@
+package io.prophecy.pipelines.testscalapipelinegit
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.testscalapipelinegit.config._
+import io.prophecy.pipelines.testscalapipelinegit.udfs.UDFs._
+import io.prophecy.pipelines.testscalapipelinegit.udfs.PipelineInitCode._
+import io.prophecy.pipelines.testscalapipelinegit.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("TestScalaPipelineGit")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf
+      .set("prophecy.metadata.pipeline.uri", "pipelines/TestScalaPipelineGit")
+    registerUDFs(spark)
+    MetricsCollector.instrument(spark, "pipelines/TestScalaPipelineGit") {
+      apply(context)
+    }
+  }
+
+}
